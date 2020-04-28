25437590538ce6429b5622b7285c9b648e4cd42a
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-28 17:27:56 +0900

    add github actions for backup

---
+++ .github/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url="https://ymmmtym.growi.cloud"
+
+for path in $(curl -sL "${url}/_api/pages.list?path=/" | jq -r '.pages[].path'); do
+    dir=$(echo ${path%/*} | cut -c 2-)
+    file=$(echo "${path#*/}.md" | nkf -w --url-input)
+    if [ ${dir} ];then
+        mkdir -p ${dir}
+    fi
+    curl -sL "${url}/_api/pages.get?path=${path}" | jq -r '.page.revision.body' > "${file}"
+done
