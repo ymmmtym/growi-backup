1e91255b13d182b12dd1a613bad3dac6afc97606
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 18:45:01 +0900

    modify scripts about appying decode dir

--- .github/backup.sh
+++ .github/backup.sh
@@ -7,7 +7,7 @@ ls | grep -v -E "^README.md" | xargs rm -rf
 
 # get markdown files
 for path in $(curl -sL "${url}/_api/pages.list?path=/" | jq -r '.pages[].path'); do
-    dir=$(echo ${path%/*} | cut -c 2-)
+    dir=$(echo ${path%/*} | cut -c 2- | nkf -w --url-input)
     file=$(echo "${path#*/}.md" | nkf -w --url-input)
     if [ ${dir} ];then
         mkdir -p ${dir}
