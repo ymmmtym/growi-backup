70317b73c3db8f8c49dfced29da7665dd1dcca6e
ymmmtym <h.yukihisa.378@gmail.com>
2020-05-08 05:35:15 +0900

    Closes #1

--- .github/backup.sh
+++ .github/backup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-export url="https://ymmmtym.growi.cloud"
+export url=$1
+
+# check http_code
+function check_http_code () {
+    http_code="$(curl -s $1 -o /dev/null -w %{http_code} --connect-timeout 60)"
+    echo ${http_code}
+}
 
 # delete without readme
 ls | grep -v -E "^README.md" | xargs rm -rf
@@ -14,6 +20,11 @@ for path in $(curl -sL "${url}/_api/pages.list?path=/" | jq -r '.pages[].path');
     fi
     if [ "${file}" = ".md" ];then
         file="index.md"
+    fi
+    http_code=$(check_http_code ${url})
+    if [ "${http_code}" != "200" ];then
+        echo "http_code is ${http_code}"
+        exit 1
     fi
     curl -sL "${url}/_api/pages.get?path=${path}" | jq -r '.page.revision.body' > "${file}"
 done
