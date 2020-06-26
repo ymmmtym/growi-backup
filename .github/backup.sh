57a172967ec9d97962d93926c7132495bb56db1d
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2020-06-26 18:04:20 +0900

    Closes #9, delete test and fix up

--- .github/backup.sh
+++ .github/backup.sh
@@ -5,13 +5,11 @@ export url=$1
 # delete without ignore files
 ls | grep -v -E "^README\.md|^.*\.py|^requirements\.txt" | xargs rm -rf
 
-# test
-test=$(ls fugafuga) || exit 1
-
 # get markdown files
-export pathes=$(curl -sL "${url}/_api/pages.list?limit=10000&path=/" | jq -r '.pages[].path' | sort) || exit 1
+export pathes=$(curl -sL "${url}/_api/pages.list?limit=10000&path=/" | jq -r '.pages[].path') || exit 1
+export sorted_pathes=$(echo ${pathes} | sort)
 
-for path in ${pathes}; do
+for path in ${sorted_pathes}; do
     dir=$(echo ${path%/*} | cut -c 2- | nkf -w --url-input)
     file=$(echo "${path#*/}.md" | nkf -w --url-input)
     if [ ${dir} ];then
